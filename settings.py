cc498461d988de89c48874aef0b54cbde4e55ec4
liam-newberry <126901253+liam-newberry@users.noreply.github.com>
2023-03-09 12:04:08 -0800

    Add files via upload
    first push

---
+++ settings.py
@@ -0,0 +1,15 @@
+# File created by: Liam Newberry
+
+WIDTH = 800
+HEIGHT = 600
+
+
+FPS = 30
+RUNNING = True
+
+PLAYER_ACC = 2
+PLAYER_FRICTION = -0.12
+
+BLACK = (0,0,0)
+BLUE = (50,50,255)
+RED = (255,50,50)
